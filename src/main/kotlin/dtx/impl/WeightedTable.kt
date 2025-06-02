a977f8967890fc7890fc81f12e435cb79f27d163
Raye W <nbness1337@gmail.com>
2025-06-02 02:34:39 -0600

    Updated WeightedTable and it's builder
    WeightedTable is now an interface and the default implementation is the class named WeightedTableImpl

--- src/main/kotlin/dtx/impl/WeightedTable.kt
+++ src/main/kotlin/dtx/impl/WeightedTable.kt
@@ -7,83 +7,30 @@ import dtx.core.SingleRollableBuilder
 import dtx.core.Rollable.Companion.defaultOnSelect
 import dtx.core.Rollable.Companion.defaultGetBaseDropRate
 import dtx.core.singleRollable
+import dtx.table.AbstractTableBuilder
 import dtx.table.Table
 import dtx.table.Table.Companion.defaultRollModifier
 import dtx.util.NoTransform
 import kotlin.random.Random
+import kotlin.sequences.forEach
 
+public interface WeightedTable<T, R>: Table<T, R> {
 
-public open class WeightedTable<T, R>(
-    public val tableIdentifier: String,
-    entries: List<WeightedRollable<T, R>>,
-    override val ignoreModifier: Boolean = false,
-    protected val rollModifierFunc: (Double) -> Double = ::defaultRollModifier,
-    protected val getTargetDropRate: (T) -> Double = ::defaultGetBaseDropRate,
-    protected open val onSelectFunc: (T, RollResult<R>) -> Unit = ::defaultOnSelect
-): Table<T, R> {
+    public override val tableEntries: List<WeightedRollable<T, R>>
 
-    init {
+    public val maxRoll: Double
 
-        require(entries.isNotEmpty()) {
-            "WeightedTable[$tableIdentifier] entries must not be empty"
-        }
+    public fun <E: WeightedRollable<T, R>> getWeightedEntry(
+        modifier: Double,
+        rolled: Double,
+        byEntries: Collection<E>,
+        selector: (E) -> Double = WeightedRollable<T, R>::weight
+    ): WeightedRollable<T, R> {
 
-        require(entries.distinctBy { it.weight }.size == entries.size) {
-            "WeightedTable[$tableIdentifier] entries must not have duplicate weights"
-        }
-    }
+        var rolledWeight = rolled
 
-
-    protected var weightSum: Double = entries.sumOf { it.weight }
-
-
-    public override val tableEntries: List<WeightedRollable<T, R>> = entries.map(NoTransform())
-
-
-    public override fun getBaseDropRate(target: T): Double {
-        return getTargetDropRate(target)
-    }
-
-
-    override fun rollModifier(percentage: Double): Double {
-
-        if (ignoreModifier) {
-            return 1.0
-        }
-
-        return rollModifierFunc(percentage)
-    }
-
-
-    public override fun onSelect(target: T, result: RollResult<R>): Unit {
-        return onSelectFunc(target, result)
-    }
-
-    protected fun checkLowEntries(): Pair<Boolean, WeightedRollable<T, R>> {
-
-        if (tableEntries.isEmpty()) {
-            return true to WeightedRollable.Empty()
-        }
-
-        if (tableEntries.size == 1) {
-
-            val singleResult = tableEntries.first()
-
-            return true to singleResult
-        }
-
-        return false to WeightedRollable.Empty()
-    }
-
-    protected fun <E: WeightedRollable<T, R>> getWeightedEntry(
-        rollMod: Double,
-        rolledWeight: Double,
-        usingEntries: Collection<E>
-    ): E {
-        var rolledWeight = rolledWeight
-
-        usingEntries.asSequence()
-            .map { entry -> (entry.weight * rollMod) to entry }
+        byEntries.asSequence()
+            .map { entry -> (selector(entry) * modifier) to entry }
             .forEach { (weight, item) ->
 
                 rolledWeight -= weight
@@ -93,24 +40,60 @@ public open class WeightedTable<T, R>(
                 }
             }
 
-        return usingEntries.last()
+        return byEntries.last()
+    }
+}
+
+public open class WeightedTableImpl<T, R>(
+    public val tableIdentifier: String,
+    entries: List<WeightedRollable<T, R>>,
+    protected val rollModifierFunc: (Double) -> Double = ::defaultRollModifier,
+    protected val getTargetDropRate: (T) -> Double = ::defaultGetBaseDropRate,
+    protected open val onSelectFunc: (T, RollResult<R>) -> Unit = ::defaultOnSelect
+): WeightedTable<T, R> {
+
+
+    override var maxRoll: Double = entries.sumOf { it.weight }
+    protected set
+
+    public override val tableEntries: List<WeightedRollable<T, R>> = entries.map(NoTransform())
+
+    public override fun getBaseDropRate(target: T): Double {
+        return getTargetDropRate(target)
+    }
+
+    override fun rollModifier(percentage: Double): Double {
+        return rollModifierFunc(percentage)
     }
 
 
+    public override fun onSelect(target: T, result: RollResult<R>): Unit {
+        return onSelectFunc(target, result)
+    }
+
     public override fun roll(target: T, otherArgs: ArgMap): RollResult<R> {
 
-        val lowEntries = checkLowEntries()
+        when (tableEntries.size) {
 
-        if (lowEntries.first) {
+            0 -> {
 
-            val result = lowEntries.second.roll(target, otherArgs)
-            onSelect(target, result)
+                val nothing = RollResult.Nothing<R>()
+                onSelect(target, nothing)
 
-            return result
+                return nothing
+            }
+
+            1 -> {
+
+                val single = tableEntries.first().roll(target, otherArgs)
+                onSelect(target, single)
+
+                return single
+            }
         }
 
         val rollMod = rollModifier(getBaseDropRate(target))
-        val rolledWeight = Random.nextDouble(0.0, weightSum)
+        val rolledWeight = Random.nextDouble(0.0, maxRoll)
         val pickedEntry = getWeightedEntry(rollMod, rolledWeight, tableEntries)
         val result = pickedEntry.roll(target, otherArgs)
         onSelect(target, result)
@@ -118,82 +101,31 @@ public open class WeightedTable<T, R>(
         return result
     }
 
-
     public override fun toString(): String = "WeightedTable[$tableIdentifier]"
+
+    init {
+        require(entries.distinctBy { it.weight }.size == entries.size) {
+            "WeightedTable[$tableIdentifier] entries must not have duplicate weights"
+        }
+
+        require(entries.isNotEmpty()) {
+            "WeightedTable[$tableIdentifier] entries must not be empty"
+        }
+    }
 }
 
 
-public open class WeightedTableBuilder<T, R> {
+public open class WeightedTableBuilder<T, R>: AbstractTableBuilder<T, R, WeightedTable<T, R>, WeightedRollable<T, R>, WeightedTableBuilder<T, R>>() {
 
-
-    public var tableName: String = "Unnamed Weighted Drop Table"
-
-
-    public var ignoreModifier: Boolean = false
-
-
-    public var onSelectFunc: (T, RollResult<R>) -> Unit = ::defaultOnSelect
-
-
-    public var targetDropRateFunc: (T) -> Double = { 1.0 }
-
-
-    public var rollModFunc: (Double) -> Double = ::defaultRollModifier
-
-
-    private val entries: MutableList<WeightedRollable<T, R>> = mutableListOf()
-
-
-    public fun onSelect(block: (T, RollResult<R>) -> Unit): WeightedTableBuilder<T, R> {
-
-        onSelectFunc = block
-
-        return this
-    }
-
-
-    public fun rollmodifier(block: (Double) -> Double): WeightedTableBuilder<T, R> {
-
-        rollModFunc = block
-
-        return this
-    }
-
-
-    public fun targetDropRate(block: (T) -> Double): WeightedTableBuilder<T, R> {
-
-        targetDropRateFunc = block
-
-        return this
-    }
-
-
-    public fun name(string: String): WeightedTableBuilder<T, R> {
-
-        tableName = string
-
-        return this
-    }
-
-
-    public fun ignoreModifier(ignore: Boolean): WeightedTableBuilder<T, R> {
-
-        ignoreModifier = ignore
-
-        return this
-    }
+    override val entries: MutableList<WeightedRollable<T, R>> = mutableListOf()
 
 
     public infix fun Double.weight(rollable: Rollable<T, R>): WeightedTableBuilder<T, R> {
-
-        entries.add(WeightedRollableImpl(this, rollable))
-
-        return this@WeightedTableBuilder
+        return addEntry(WeightedRollableImpl(this, rollable))
     }
 
 
     public inline infix fun Double.weight(entry: R): WeightedTableBuilder<T, R> {
-
         return weight(Rollable.Single(entry))
     }
 
@@ -218,13 +150,12 @@ public open class WeightedTableBuilder<T, R> {
     }
 
 
-    public fun build(): WeightedTable<T, R> {
-        return WeightedTable(
+    override fun build(): WeightedTable<T, R> {
+        return WeightedTableImpl(
             tableIdentifier = tableName,
             entries = entries,
-            ignoreModifier = ignoreModifier,
-            rollModifierFunc = rollModFunc,
-            getTargetDropRate = targetDropRateFunc,
+            rollModifierFunc = getRollModFunc,
+            getTargetDropRate = getDropRateFunc,
             onSelectFunc = onSelectFunc
         )
     }
