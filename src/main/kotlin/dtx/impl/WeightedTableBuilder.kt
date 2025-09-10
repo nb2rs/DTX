f73570e804335a044edde0c785028318bc2cc6b1
Raye W <nbness1337@gmail.com>
2025-09-10 05:29:16 -0600

    Updated WeightedTableBuilder

--- src/main/kotlin/dtx/impl/WeightedTableBuilder.kt
+++ src/main/kotlin/dtx/impl/WeightedTableBuilder.kt
@@ -2,64 +2,71 @@ package dtx.impl
 
 import dtx.core.ResultSelector
 import dtx.core.Rollable
+import dtx.core.Single
+import dtx.core.SingleByFun
 import dtx.core.SingleRollableBuilder
 import dtx.core.singleRollable
-import dtx.table.AbstractTableBuilder
+import dtx.table.DefaultTableBuilder
+import dtx.table.TableHooks
 
-public open class WeightedTableBuilder<T, R>: AbstractTableBuilder<T, R, WeightedTable<T, R>, WeightedRollable<T, R>, WeightedTableBuilder<T, R>>() {
+public open class WeightedTableBuilder<T, R, TT: WeightedTable<T, R>>(
+    internal val impl: (String, Collection<WeightedRollable<T, R>>, TableHooks<T, R>) -> TT
+): DefaultTableBuilder<T, R, WeightedRollable<T, R>, TT>() {
 
-    override val entries: MutableList<WeightedRollable<T, R>> = mutableListOf()
-
-    public infix fun Double.weight(rollable: Rollable<T, R>): WeightedTableBuilder<T, R> {
-        return addEntry(WeightedRollableImpl(this, rollable))
+    init {
+        construct {
+            impl(
+                tableIdentifier,
+                weightedEntries,
+                hooks.build()
+            )
+        }
     }
 
-    public inline infix fun Double.weight(entry: R): WeightedTableBuilder<T, R> {
-        return weight(Rollable.Single(entry))
+    override val entries: MutableCollection<WeightedRollable<T, R>> = mutableListOf()
+    private val weightedEntries: MutableList<WeightedRollable<T, R>> = mutableListOf()
+
+    public infix fun Double.weight(rollable: Rollable<T, R>): WeightedTableBuilder<T, R, TT> {
+        val weightedRollable = WeightedRollableImpl(this, rollable)
+        weightedEntries.add(weightedRollable)
+        return addEntry(weightedRollable) as WeightedTableBuilder<T, R, TT>
     }
 
-    public infix fun Double.weightBy(selector: ResultSelector<T, R>): WeightedTableBuilder<T, R> {
-        return weight(Rollable.SingleByFun(selector))
+    public inline infix fun Double.weight(entry: R): WeightedTableBuilder<T, R, TT> {
+        return weight(Single(entry))
     }
 
-    public infix fun Double.weight(block: SingleRollableBuilder<T, R>.() -> Unit): WeightedTableBuilder<T, R> {
+    public infix fun Double.weightBy(selector: ResultSelector<T, R>): WeightedTableBuilder<T, R, TT> {
+        return weight(SingleByFun(selector))
+    }
+
+    public infix fun Double.weight(block: SingleRollableBuilder<T, R>.() -> Unit): WeightedTableBuilder<T, R, TT> {
         return weight(singleRollable(block))
     }
 
-    public inline infix fun Int.weight(rollable: Rollable<T, R>): WeightedTableBuilder<T, R>{
+    public inline infix fun Int.weight(rollable: Rollable<T, R>): WeightedTableBuilder<T, R, TT>{
         return toDouble().weight(rollable)
     }
 
-    public inline infix fun Int.weight(item: R): WeightedTableBuilder<T, R> {
-        return weight(Rollable.Single(item))
+    public inline infix fun Int.weight(item: R): WeightedTableBuilder<T, R, TT> {
+        return weight(Single(item))
     }
 
-    public infix fun Int.weightBy(selector: ResultSelector<T, R>): WeightedTableBuilder<T, R> {
+    public infix fun Int.weightBy(selector: ResultSelector<T, R>): WeightedTableBuilder<T, R, TT> {
         return toDouble().weightBy(selector)
     }
 
-    public infix fun Int.weight(block: SingleRollableBuilder<T, R>.() -> Unit): WeightedTableBuilder<T, R> {
+    public infix fun Int.weight(block: SingleRollableBuilder<T, R>.() -> Unit): WeightedTableBuilder<T, R, TT> {
         return toDouble().weight(block)
-    }
-
-    override fun build(): WeightedTable<T, R> {
-        return WeightedTableImpl(
-            tableIdentifier = tableName,
-            entries = entries,
-            shouldRollFunc = shouldRollFunc,
-            rollModifierFunc = getRollModFunc,
-            getTargetDropRate = getDropRateFunc,
-            onSelectFunc = onSelectFunc
-        )
     }
 }
 
 public fun <T, R> weightedTable(
     tableName: String = "Unnamed Weighted Table",
-    block: WeightedTableBuilder<T, R>.() -> Unit
+    block: WeightedTableBuilder<T, R, WeightedTable<T, R>>.() -> Unit
 ): WeightedTable<T, R> {
 
-    val builder = WeightedTableBuilder<T, R>()
+    val builder = WeightedTableBuilder<T, R, WeightedTable<T, R>> { identifier, entries, hooks -> WeightedTableImpl(identifier, entries.toList(), hooks) }
     builder.apply { name(tableName) }
     builder.apply(block)
 
