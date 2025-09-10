704435a73ffa7205cab63a3780b6e2eaa172de1d
Raye W <nbness1337@gmail.com>
2025-09-10 05:17:13 -0600

    RollableImpl updated
    re-implements the Rollable changes

--- src/main/kotlin/dtx/core/RollableImpl.kt
+++ src/main/kotlin/dtx/core/RollableImpl.kt
@@ -2,115 +2,44 @@ package dtx.core
 
 public data class AnyOf<T, R>(
     public val rollables: List<Rollable<T, R>>,
-    public val predicate: ShouldRoll<T> = ::defaultShouldRoll,
-    public val onSelectFun: OnSelect<T, R> = Rollable.Companion::defaultOnSelect
-): Rollable<T, R> {
+    public val withHooks: RollableHooks<T, R> = RollableHooks.Default()
+): Rollable<T, R>, RollableHooks<T, R> by withHooks {
 
-    override fun shouldRoll(target: T): Boolean {
-        return predicate.invoke(target)
-    }
-
-    override fun onSelect(target: T, result: RollResult<R>) {
-        return onSelectFun.invoke(target, result)
-    }
-
-    override fun roll(target: T, otherArgs: ArgMap): RollResult<R> {
-
-        if (!shouldRoll(target)) {
-            return RollResult.Nothing()
-        }
-
-        val result = rollables.random().roll(target, otherArgs)
-        onSelect(target, result)
-
-        return result
+    override fun selectResult(target: T, otherArgs: ArgMap): RollResult<R> {
+        return rollables.random().roll(target, otherArgs)
     }
 }
 
 public data class AllOf<T, R>(
     public val rollables: List<Rollable<T, R>>,
-    public val shouldRollFunc: ShouldRoll<T> = ::defaultShouldRoll,
-    public val onSelectFun: OnSelect<T, R> = Rollable.Companion::defaultOnSelect
-): Rollable<T, R> {
+    public val withHooks: RollableHooks<T, R> = RollableHooks.Default()
+): Rollable<T, R>, RollableHooks<T, R> by withHooks {
 
-    override fun shouldRoll(target: T): Boolean {
-        return shouldRollFunc(target)
-    }
-
-    override fun onSelect(target: T, result: RollResult<R>) {
-        return onSelectFun.invoke(target, result)
-    }
-
-    override fun roll(target: T, otherArgs: ArgMap): RollResult<R> {
-
-        if (!shouldRoll(target)) {
-            return RollResult.Nothing()
-        }
-
-        val results = buildList {
-
-            rollables.forEach { rollable ->
-                add(rollable.roll(target, otherArgs))
-            }
-        }
-
-        val result = results.flattenToList()
-        onSelect(target, result)
-
-        return result
+    override fun selectResult(target: T, otherArgs: ArgMap): RollResult<R> {
+        return rollables
+            .map { it.roll(target, otherArgs) }
+            .flattenToList()
     }
 }
 
 public data class Single<T, R>(
     public val result: R,
-    public val shouldRollFunc: ShouldRoll<T> = ::defaultShouldRoll,
-    public val onSelectFun: OnSelect<T, R> = Rollable.Companion::defaultOnSelect
-): Rollable<T, R> {
+    public val withHooks: RollableHooks<T, R> = RollableHooks.Default()
+): Rollable<T, R>, RollableHooks<T, R> by withHooks {
 
-    override fun shouldRoll(target: T): Boolean {
-        return shouldRollFunc(target)
-    }
+    private val retResult = RollResult.Single<R>(result)
 
-    override fun onSelect(target: T, result: RollResult<R>) {
-        return onSelectFun.invoke(target, result)
-    }
-
-    override fun roll(target: T, otherArgs: ArgMap): RollResult<R> {
-
-        if (!shouldRoll(target)){
-            return RollResult.Nothing()
-        }
-
-        val result = RollResult.Single(result)
-        onSelect(target, result)
-
-        return result
+    override fun selectResult(target: T, otherArgs: ArgMap): RollResult<R> {
+        return retResult
     }
 }
 
 public class SingleByFun<T, R>(
     public val resultSelector: ResultSelector<T, R>,
-    public val shouldRollFunc: ShouldRoll<T> = ::defaultShouldRoll,
-    public val onSelectFun: OnSelect<T, R> = Rollable.Companion::defaultOnSelect
-): Rollable<T, R> {
+    public val withHooks: RollableHooks<T, R> = RollableHooks.Default()
+): Rollable<T, R>, RollableHooks<T, R> by withHooks {
 
-    override fun shouldRoll(target: T): Boolean {
-        return shouldRollFunc(target)
-    }
-
-    override fun onSelect(target: T, result: RollResult<R>) {
-        return onSelectFun.invoke(target, result)
-    }
-
-    override fun roll(target: T, otherArgs: ArgMap): RollResult<R> {
-
-        if (!shouldRoll(target)) {
-            return RollResult.Nothing()
-        }
-
-        val result = RollResult.Single(resultSelector.invoke(target))
-        onSelect(target, result)
-
-        return result
+    override fun selectResult(target: T, otherArgs: ArgMap): RollResult<R> {
+        return resultSelector(target, otherArgs)
     }
 }
